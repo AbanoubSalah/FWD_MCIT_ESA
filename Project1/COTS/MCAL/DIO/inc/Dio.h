7b93be90aac9336ab12b5fe89659878a8fb9eb0b
Abanoub Salah <Abanoub.S.Mitry@gmail.com>
2022-08-26 03:17:12 +0200

    Implemented MCAL Dio driver

--- Project1/COTS/MCAL/DIO/inc/Dio.h
+++ Project1/COTS/MCAL/DIO/inc/Dio.h
@@ -13,6 +13,8 @@
  * INCLUDES
  *********************************************************************************************************************/
 
+#include "Dio_Types.h"
+
 /**********************************************************************************************************************
  *  GLOBAL CONSTANT MACROS
  *********************************************************************************************************************/
@@ -33,6 +35,64 @@
  *  GLOBAL FUNCTION PROTOTYPES
  *********************************************************************************************************************/
  
+ /******************************************************************************
+* @brief Read a DIO pin
+*
+* Reads a DIO pin. This function is synchronous and re-entrant
+*
+* @param ChannelID			The ID of the pin to be read
+*
+* @returns DIO_LevelType either  DIO_LEVEL_LOW /
+*                                DIO_LEVEL_HIGH                                  
+*******************************************************************************/
+DIO_LevelType DIO_ReadChannel(DIO_ChannelType ChannelID);
+
+/******************************************************************************
+* @brief Writes to a DIO pin
+*
+* Writes to a DIO pin. This function is synchronous and re-entrant
+*
+* @param ChannelID			The channel ID to write to it
+* @param Level				The pin level to be written to
+*
+* @returns None
+*******************************************************************************/
+void DIO_WriteChannel(DIO_ChannelType ChannelID, DIO_LevelType Level);
+
+/******************************************************************************
+* @brief Read an entire port
+*
+* Reads a DIO port. This function is synchronous and re-entrant
+*
+* @param PortID				The ID of the port to be read
+*
+* @returns DIO port level
+*******************************************************************************/
+DIO_PortLevelType DIO_ReadPort(DIO_PortType PortID);
+
+/******************************************************************************
+* @brief Writes to an entire port                                  
+*
+* Writes to a DIO port. This function is synchronous and re-entrant
+*                                                                             
+* @param PortId				The ID of the port to be written to
+* @param Level				The port level to be written to
+*
+* @returns None
+*******************************************************************************/
+void DIO_WritePort(DIO_PortType PortID, DIO_PortLevelType Level);
+
+/******************************************************************************
+* @brief Flips the value of a pin
+*
+* Flips the value of a specific pin. This function is synchronous and re-entrant
+*
+* @param ChannelId   the id of the port to be flipped                
+*
+* @returns None
+*******************************************************************************/
+void DIO_FlipChannel(DIO_ChannelType ChannelID);
+
 #endif  /* _DIO_H */
 
 /**********************************************************************************************************************
--- Project1/COTS/MCAL/DIO/src/Dio.c
+++ Project1/COTS/MCAL/DIO/src/Dio.c
@@ -10,6 +10,9 @@
  *  INCLUDES
  *********************************************************************************************************************/
 
+#include "Dio.h"
+#include "Common_Macros.h"
+
 /**********************************************************************************************************************
 *  LOCAL MACROS CONSTANT\FUNCTION
 *********************************************************************************************************************/
@@ -17,6 +20,15 @@
 /**********************************************************************************************************************
  *  LOCAL DATA 
  *********************************************************************************************************************/
+
+static const uint32 portBase[] = {
+    PORTA_APB_BASE,
+    PORTB_APB_BASE,
+    PORTC_APB_BASE,
+    PORTD_APB_BASE,
+    PORTE_APB_BASE,
+    PORTF_APB_BASE,
+};
 
 /**********************************************************************************************************************
  *  GLOBAL DATA
@@ -33,6 +45,47 @@
 /**********************************************************************************************************************
  *  GLOBAL FUNCTIONS
  *********************************************************************************************************************/
+
+DIO_LevelType DIO_ReadChannel(DIO_ChannelType ChannelID)
+{
+	uint8 portNumber = ChannelID / 8;
+    uint8 pinPosition = ChannelID % 8;
+    DIO_LevelType pinLevel;
+
+    return (DIO_LevelType)GET_BITBAND_ALIAS_ADDRESS((portBase[portNumber] + GPIODATA_ALL_PINS_MASK_OFFSET), pinPosition);
+}
+
+void DIO_WriteChannel(DIO_ChannelType ChannelID, DIO_LevelType Level)
+{
+	uint8 portNumber = ChannelID / 8;
+    uint8 pinPosition = ChannelID % 8;
+
+    GET_BITBAND_ALIAS_ADDRESS((portBase[portNumber] + GPIODATA_ALL_PINS_MASK_OFFSET), pinPosition) = Level;
+
+	return ;
+}
+
+DIO_PortLevelType DIO_ReadPort(DIO_PortType PortID)
+{
+    return (DIO_PortLevelType)GET_ADDRESS_BASE_OFFSET(portBase[PortID], GPIODATA_ALL_PINS_MASK_OFFSET);
+}
+
+void DIO_WritePort(DIO_PortType PortID, DIO_PortLevelType Level)
+{
+	GET_ADDRESS_BASE_OFFSET(portBase[PortID], GPIODATA_ALL_PINS_MASK_OFFSET) = Level;
+	
+	return ;
+}
+
+void DIO_FlipChannel(DIO_ChannelType ChannelID)
+{
+    uint8 portNumber = ChannelID / 8;
+    uint8 pinPosition = ChannelID % 8;
+
+	GET_BITBAND_ALIAS_ADDRESS((portBase[portNumber] + GPIODATA_ALL_PINS_MASK_OFFSET), pinPosition) = (GET_BITBAND_ALIAS_ADDRESS((portBase[portNumber] + GPIODATA_ALL_PINS_MASK_OFFSET), pinPosition) ^ STD_HIGH);
+	
+	return ;
+}
 
 /**********************************************************************************************************************
  *  END OF FILE: Dio.c
